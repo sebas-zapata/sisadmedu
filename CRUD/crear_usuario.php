518884710149deb9f8962aa18c734832ec258308
brandon adrian <lenadevalderrama@gmail.com>
2024-11-16 19:29:23 -0500

    coloque una validacion en crear_usuario.php para datos duplicados y modifique el codigo de registro-usuarios.php para solucionar un problema de validacion

--- CRUD/crear_usuario.php
+++ CRUD/crear_usuario.php
@@ -6,7 +6,8 @@ if (!$conex) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
+
+    // Recupera los valores de los campos del formulario
     $tipo_documento = mysqli_real_escape_string($conex, $_POST['tipo_documento_codigo_tipo_documento']);
     $documento_usuario = mysqli_real_escape_string($conex, $_POST['documento_usuario']);
     $nombres_usuario = mysqli_real_escape_string($conex, $_POST['nombres_usuario']);
@@ -16,24 +17,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $contraseña_usuario = mysqli_real_escape_string($conex, $_POST['contrasena_usuario']);
     $contraseña_usuario = hash('sha512', $contraseña_usuario);
     $id_rol = mysqli_real_escape_string($conex, $_POST['id_rol']);
+    $id_grupo = mysqli_real_escape_string($conex, $_POST['id_grupo']);
 
-    
+    // Verificar si el correo electrónico ya existe
     $consulta_correo = "SELECT * FROM usuarios WHERE correo_electronico_usuario = '$correo_electronico_usuario'";
     $resultado_correo = mysqli_query($conex, $consulta_correo);
 
     if (mysqli_num_rows($resultado_correo) > 0) {
         echo "<script>alert('El correo ya existe. Por favor, ingrese otro.');window.location='registro-usuarios.php'</script>";
     } else {
-       
-        $insertar = "INSERT INTO usuarios (`tipo_documento_codigo_tipo_documento`, `documento_usuario`, `nombres_usuario`, `apellidos_usuario`, `correo_electronico_usuario`, `telefono_usuario`, `contrasena_usuario`, `rol_id_rol1`) VALUES ('$tipo_documento', '$documento_usuario', '$nombres_usuario', '$apellidos_usuario', '$correo_electronico_usuario', '$telefono_usuario', '$contraseña_usuario', '$id_rol')";
-        $resultado = mysqli_query($conex, $insertar);
+        // Verificar si el documento ya existe
+        $consulta_documento = "SELECT * FROM usuarios WHERE documento_usuario = '$documento_usuario'";
+        $resultado_documento = mysqli_query($conex, $consulta_documento);
 
-        if ($resultado) {
-            header("location: usuarios.php"); 
-            exit();
+        if (mysqli_num_rows($resultado_documento) > 0) {
+            echo "<script>alert('El documento ya existe. Por favor, ingrese otro.');window.location='registro-usuarios.php'</script>";
         } else {
-            echo 'Error, no se pudo crear la cuenta';
+            // Verificar si el teléfono ya existe
+            $consulta_telefono = "SELECT * FROM usuarios WHERE telefono_usuario = '$telefono_usuario'";
+            $resultado_telefono = mysqli_query($conex, $consulta_telefono);
+
+            if (mysqli_num_rows($resultado_telefono) > 0) {
+                echo "<script>alert('El teléfono ya existe. Por favor, ingrese otro.');window.location='registro-usuarios.php'</script>";
+            } else {
+                // Insertar datos del usuario en la base de datos
+                $insertar = "INSERT INTO usuarios (`tipo_documento_codigo_tipo_documento`, `documento_usuario`, `nombres_usuario`, `apellidos_usuario`, `correo_electronico_usuario`, `telefono_usuario`, `contrasena_usuario`, `rol_id_rol1`, `grupo_id_grupo`) VALUES ('$tipo_documento', '$documento_usuario', '$nombres_usuario', '$apellidos_usuario', '$correo_electronico_usuario', '$telefono_usuario', '$contraseña_usuario', '$id_rol', '$id_grupo')";
+                $resultado = mysqli_query($conex, $insertar);
+
+                if ($resultado) {
+                    header("location: usuarios.php");
+                    exit();
+                } else {
+                    echo 'Error, no se pudo crear la cuenta';
+                }
+            }
         }
     }
 }
 ?>
+
--- CRUD/registro-usuarios.php
+++ CRUD/registro-usuarios.php
@@ -18,17 +18,19 @@ $conex = mysqli_connect("localhost", "root", "", "sisadmedu");
 
 <body>
     <div class="container mt-2">
-        
+
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <!-- Formulario -->
                 <form class="formulario-registro p-4 rounded m-2" id="registroForm" action="crear_usuario.php" method="POST" autocomplete="off">
-                <a class="btn btn-volver" href="./usuarios.php">Volver</a>
-                    <h2 class="text-center text-light mb-5">Registro de Usuarios</h2> <!-- Título agregado -->
+                    <a class="btn btn-volver" href="./usuarios.php">Volver</a>
+                    <h2 class="text-center text-light mb-5">Registro de Usuarios</h2>
+
                     <!-- Selección de Tipo de Documento -->
                     <div class="mb-3">
                         <label for="tipo_documento" class="form-label text-light">Tipo de Documento</label>
-                        <select class="form-select" name="tipo_documento_codigo_tipo_documento" id="tipo_documento" required>
+                        <select class="form-select" name="tipo_documento_codigo_tipo_documento" id="tipo_documento">
+                            <option value="" selected>Seleccione una opción</option>
                             <?php
                             $consulta = "SELECT codigo_tipo_documento, descripcion_tipo_documento FROM tipo_documento";
                             $resultado = mysqli_query($conex, $consulta);
@@ -41,54 +43,56 @@ $conex = mysqli_connect("localhost", "root", "", "sisadmedu");
                             }
                             ?>
                         </select>
+                        <span class="text-danger small error" id="tipoDocumentoError"></span>
                     </div>
 
                     <!-- Número de Documento -->
                     <div class="mb-3">
                         <label for="documento" class="form-label text-light">Número de Documento</label>
                         <input type="text" class="form-control" id="documento" name="documento_usuario" placeholder="Número de documento">
-                        <span class="text-danger small" id="docError"></span>
+                        <span class="text-danger small error" id="docError"></span>
                     </div>
 
                     <!-- Nombres -->
                     <div class="mb-3">
                         <label for="nombre" class="form-label text-light">Nombres</label>
                         <input type="text" class="form-control" id="nombre" name="nombres_usuario" placeholder="Nombres">
-                        <span class="text-danger small" id="nombreError"></span>
+                        <span class="text-danger small error" id="nombreError"></span>
                     </div>
 
                     <!-- Apellidos -->
                     <div class="mb-3">
                         <label for="apellidos" class="form-label text-light">Apellidos</label>
                         <input type="text" class="form-control" id="apellidos" name="apellidos_usuario" placeholder="Apellidos">
-                        <span class="text-danger small" id="apellidosError"></span>
+                        <span class="text-danger small error" id="apellidosError"></span>
                     </div>
 
                     <!-- Correo Electrónico -->
                     <div class="mb-3">
                         <label for="email" class="form-label text-light">Correo Electrónico</label>
                         <input type="email" class="form-control" id="email" name="correo_electronico_usuario" placeholder="Correo electrónico">
-                        <span class="text-danger small" id="emailError"></span>
+                        <span class="text-danger small error" id="emailError"></span>
                     </div>
 
                     <!-- Número de Teléfono -->
                     <div class="mb-3">
                         <label for="telefono" class="form-label text-light">Número de Teléfono</label>
                         <input type="tel" class="form-control" id="telefono" name="telefono_usuario" placeholder="Número de teléfono">
-                        <span class="text-danger small" id="telefonoError"></span>
+                        <span class="text-danger small error" id="telefonoError"></span>
                     </div>
 
                     <!-- Contraseña -->
                     <div class="mb-3">
                         <label for="password" class="form-label text-light">Contraseña</label>
                         <input type="password" class="form-control" id="password" name="contrasena_usuario" placeholder="Contraseña">
-                        <span class="text-danger small" id="passwordError"></span>
+                        <span class="text-danger small error" id="passwordError"></span>
                     </div>
 
                     <!-- Selección de Rol -->
                     <div class="mb-3">
                         <label for="rol" class="form-label text-light">Rol</label>
                         <select class="form-select" id="rol" name="id_rol">
+                            <option value="" selected>Seleccione una opción</option>
                             <?php
                             $consulta = "SELECT * FROM rol";
                             $resultado = mysqli_query($conex, $consulta);
@@ -101,7 +105,27 @@ $conex = mysqli_connect("localhost", "root", "", "sisadmedu");
                             }
                             ?>
                         </select>
-                        <span class="text-danger small" id="rolError"></span>
+                        <span class="text-danger small error" id="rolError"></span>
+                    </div>
+
+                    <!-- Selección de Grupo -->
+                    <div class="mb-3" id="grupoContainer" style="display: none;">
+                        <label for="grupo" class="form-label text-light">Grupo</label>
+                        <select class="form-control" name="id_grupo" id="grupo">
+
+                            <?php
+                            $consulta = "SELECT * FROM grupo";
+                            $resultado = mysqli_query($conex, $consulta);
+                            while ($id_grupo = mysqli_fetch_array($resultado)) {
+                            ?>
+                                <option value="<?php echo $id_grupo['id_grupo']; ?>">
+                                    <?php echo $id_grupo['nombre_grupo']; ?>
+                                </option>
+                            <?php
+                            }
+                            ?>
+                        </select>
+                        <span class="text-danger small error" id="grupoError"></span>
                     </div>
 
                     <!-- Botón de Enviar -->
@@ -109,10 +133,24 @@ $conex = mysqli_connect("localhost", "root", "", "sisadmedu");
                         <button type="submit" class="btn btn-guardar w-100 p-3">Registrar usuario</button>
                     </div>
                 </form>
+
             </div>
         </div>
     </div>
     <script src="../js/validar-registro-usuarios.js"></script>
+    <script>
+        const rolSelect = document.getElementById('rol');
+        const grupoContainer = document.getElementById('grupoContainer');
+
+        rolSelect.addEventListener('change', () => {
+            const selectedOption = rolSelect.options[rolSelect.selectedIndex].text.toLowerCase();
+            if (selectedOption === 'estudiante') {
+                grupoContainer.style.display = 'block';
+            } else {
+                grupoContainer.style.display = 'none';
+            }
+        });
+    </script>
 </body>
 
 </html>
