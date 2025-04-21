60af3a71578f391116ce0ac0f98d7e079d203078
Sebastian Zapata <zapatajuan351@gmail.com>
2025-04-21 09:18:35 -0500

    Modificacion de lo login

--- sisadmedu-la/resources/views/auth/login.blade.php
+++ sisadmedu-la/resources/views/auth/login.blade.php
@@ -7,36 +7,25 @@
 
         <!-- Dirección de correo electrónico -->
         <div>
-            <x-input-label for="email" :value="__('Correo electrónico')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="Correo electronico" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Contraseña -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Contraseña')" />
             <x-text-input id="password" class="block mt-1 w-full"
                 type="password"
                 name="password"
+                placeholder="Contraseña"
                 required autocomplete="current-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Recordarme -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Recuérdame') }}</span>
-            </label>
-        </div>
-
         <!-- Botones de acción -->
-        <div class="flex justify-center gap-4 mt-3">
+        <div class="flex justify-center gap-4 mt-5">
             <x-primary-button class="w-50 sm:w-auto flex justify-center items-cente">
-                {{ __('Iniciar sesión') }}
+                {{ __('Acceder') }}
             </x-primary-button>
-
-            <a href="{{ route('register') }}" class="btn-crear-cuenta">Crear cuenta</a>
         </div>
 
         <div class="flex items-center justify-between mt-4">
