44cc5bd6d56817f73b8544267cefa2c84ca86973
brandon adrian <lenadevalderrama@gmail.com>
2025-09-11 16:20:41 -0500

    hice la recuperacion de contrasena creando su controlador y las vistas consumiendo la api de sendgrid y una nueva migracion en la que agrego 2 campos mas a la tabla usuarios para el codigo de recuperacion de la contrasena

--- sisadmedu/routes/web.php
+++ sisadmedu/routes/web.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DocenteController;
 use App\Http\Controllers\GradoController;
 use App\Http\Controllers\EstudianteController;
+use App\Http\Controllers\ForgotPasswordController;
+
 
 // Rutas con el middleware de autenticación
 Route::group(['middleware' => 'auth'], function () {
@@ -20,6 +22,17 @@ Route::group(['middleware' => 'auth'], function () {
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('login.post');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Recuperación de contraseña
+Route::get('/password/forgot', [ForgotPasswordController::class, 'showEmailForm'])->name('password.forgot');
+Route::post('/password/send-code', [ForgotPasswordController::class, 'sendCode'])->name('password.send.code');
+
+Route::get('/password/verify', [ForgotPasswordController::class, 'showVerifyForm'])->name('password.verify.form');
+Route::post('/password/verify', [ForgotPasswordController::class, 'verifyCode'])->name('password.verify');
+
+Route::get('/password/reset', [ForgotPasswordController::class, 'showResetForm'])->name('password.reset.form');
+Route::post('/password/reset', [ForgotPasswordController::class, 'resetPassword'])->name('password.reset');
+
 
 // Ruta dashboard
 Route::get('/', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard');
