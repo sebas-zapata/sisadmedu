9508e03a95307118647be900d4b830d859f3e427
Sebastian Zapata <zapatajuan351@gmail.com>
2025-11-19 11:18:55 -0500

    Configuracion del Dockerfile para vincular los assets

--- sisadmedu/resources/views/auth/login.blade.php
+++ sisadmedu/resources/views/auth/login.blade.php
@@ -3,7 +3,9 @@
 
 @section('title', 'Iniciar sesión')
 
-
+@section('contenido')
+{{-- Cargar loader oculto --}}
+@include('auth.partials._loader_sistema')
 
 {{-- Mensaje de error general de autenticación --}}
 @if(session('error'))
